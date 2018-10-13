b5d8a389a9b915ee5a63dcb4d241a383cb3468ae
Alok <friend9831@gmail.com>
2018-10-13 23:27:20 +0530

    first commit

---
+++ conf.js
@@ -0,0 +1,22 @@
+
+var HtmlReporter = require('protractor-beautiful-reporter');
+
+
+exports.config = {
+	framework : 'jasmine',
+	seleniumAddress : 'http://localhost:4444/wd/hub',
+	specs : [ 'spec.js' ],
+	/*multiCapabilities : [ {
+		browserName : 'firefox'
+	}, {
+		browserName : 'chrome'
+	} ]*/
+	
+	
+	onPrepare: function() {
+	      // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+	      jasmine.getEnv().addReporter(new HtmlReporter({
+	         baseDirectory: 'reports', screenshotsSubfolder: 'images', jsonsSubfolder: 'jsons'
+	      }).getJasmine2Reporter());
+	   }
+}
